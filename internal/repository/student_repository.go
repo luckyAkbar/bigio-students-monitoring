4bae7d214c0f571992ce08f4aed96aa3d3b06dc9
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-09-16 18:19:56 +0700

    create function implementation

--- internal/repository/student_repository.go
+++ internal/repository/student_repository.go
@@ -39,6 +39,42 @@ func (r *studentRepo) FindByID(ctx context.Context, id int64) (*models.Student, 
 	}
 }
 
-func (r *studentRepo) GetGradeBySubjectID(ctx context.Context, subjectID int64) (*models.Grade, error) {
-	return nil, nil
+func (r *studentRepo) FindByStudentAndSubjectID(ctx context.Context, studentID, subjectID int64) (*models.Grade, error) {
+	logger := logrus.WithFields(logrus.Fields{
+		"ctx":       utils.DumpIncomingContext(ctx),
+		"studentID": studentID,
+		"subjectID": subjectID,
+	})
+
+	grade := &models.Grade{}
+
+	err := r.db.WithContext(ctx).Model(&models.Grade{}).
+		Where("student_id = ? AND subject_id = ?", studentID, subjectID).
+		Take(grade).Error
+
+	switch err {
+	default:
+		logger.Error(err)
+		return nil, err
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	case nil:
+		return grade, nil
+	}
+}
+
+func (r *studentRepo) GetAllGrade(ctx context.Context, id int64) (grades []models.Grade, err error) {
+	logger := logrus.WithFields(logrus.Fields{
+		"ctx": utils.DumpIncomingContext(ctx),
+	})
+
+	err = r.db.WithContext(ctx).Model(&models.Grade{}).
+		Where("student_id = ?", id).Scan(&grades).Error
+
+	if err != nil {
+		logger.Error(err)
+		return grades, err
+	}
+
+	return grades, nil
 }
