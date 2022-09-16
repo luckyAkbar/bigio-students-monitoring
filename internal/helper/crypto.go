0a6af1d96265f880ebfd3fd1038cf2fe2119dc9f
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-09-16 12:50:11 +0700

    add new functions

--- internal/helper/crypto.go
+++ internal/helper/crypto.go
@@ -1,6 +1,15 @@
 package helper
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"errors"
+	"strings"
+
+	"github.com/kumparan/go-utils"
+	"github.com/luckyAkbar/bigio-students-monitoring/internal/config"
+	"github.com/mattheath/base62"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // HashString encrypt given text
 func HashString(text string) (string, error) {
@@ -9,4 +18,29 @@ func HashString(text string) (string, error) {
 		return "", err
 	}
 	return string(bt), nil
+}
+
+func IsHashedStringMatch(plain, cipher []byte) bool {
+	err := bcrypt.CompareHashAndPassword(cipher, plain)
+	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+		return false
+	}
+	if err != nil {
+		logrus.Error(err)
+		return false
+	}
+	return true
+}
+
+func GenerateToken(uniqueID int64) string {
+	sb := strings.Builder{}
+
+	encodedID := base62.EncodeInt64(uniqueID)
+	sb.WriteString(encodedID)
+	sb.WriteString("_")
+
+	randString := utils.GenerateRandomAlphanumeric(config.DefaultTokenLength)
+	sb.WriteString(randString)
+
+	return sb.String()
 }
