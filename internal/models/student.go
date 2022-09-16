239ad9ac8d28fd33baaa15007abbff4760809377
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-09-16 16:26:54 +0700

    add validator to create student input

--- internal/models/student.go
+++ internal/models/student.go
@@ -3,12 +3,17 @@ package models
 import "context"
 
 type Student struct {
-	ID int64 `json:"id"`
+	ID   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
 type CreateStudentInput struct {
-	Name string `json:"name"`
+	Name     string `json:"name" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (i *CreateStudentInput) Validate() error {
+	return validator.Struct(i)
 }
 
 type StudentUsecase interface {
@@ -17,6 +22,6 @@ type StudentUsecase interface {
 }
 
 type StudentRepository interface {
-	GetGradeBySubjectID(ctxt context.Context, subjectID int64) (int, error)
+	GetGradeBySubjectID(ctxt context.Context, subjectID int64) (*Grade, error)
 	FindByID(ctxt context.Context, id int64) (*Student, error)
 }
