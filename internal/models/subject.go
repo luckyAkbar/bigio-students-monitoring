9309aff4e3311d4f4357877e49eddce68e169424
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-09-16 16:27:23 +0700

    add validator to input

--- internal/models/subject.go
+++ internal/models/subject.go
@@ -3,16 +3,21 @@ package models
 import "context"
 
 type Subject struct {
-	ID int64 `json:"id"`
-	Name string `json:"name"`
-	TeacherID int64 `json:"teacher_id"`
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	TeacherID int64  `json:"teacher_id"`
 }
 
 type CreateSubjectInput struct {
-	Name string `json:"name"`
-	TeacherID int64 `json:"teacher_id"`
+	Name      string `json:"name" validate:"required"`
+	TeacherID int64  `json:"teacher_id" validate:"required"`
+}
+
+func (i *CreateSubjectInput) Validate() error {
+	return validator.Struct(i)
 }
 
 type SubjectRepository interface {
 	FindByID(ctx context.Context, id int64) (*Subject, error)
+	Create(ctx context.Context, subject *Subject) error
 }
--- internal/models/teacher.go
+++ internal/models/teacher.go
@@ -3,17 +3,22 @@ package models
 import "context"
 
 type Teacher struct {
-	ID int64 `json:"id"`
+	ID   int64  `json:"id"`
 	Name string `json:"name"`
 }
 
 type CreateTeacherInput struct {
-	Name string `json:"name"`
+	Name     string `json:"name" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func (i *CreateTeacherInput) Validate() error {
+	return validator.Struct(i)
 }
 
 type TeacherUsecase interface {
 	FindByID(ctx context.Context, id int64) (*Teacher, error)
-	GradeByStudentID(ctx context.Context, input *CreateGradeInput) error
+	GradeByStudentID(ctx context.Context, input *CreateGradeInput) (*Grade, error)
 }
 
 type TeacherRepository interface {
