28f1b17b0a107c33856beacfc0d5457852b2caf2
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-09-16 12:50:52 +0700

    add new function and interface method

--- internal/models/session.go
+++ internal/models/session.go
@@ -3,19 +3,31 @@ package models
 import (
 	"context"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 type Session struct {
-	ID int64 `json:"-"`
-	AccessToken string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	UserID int64 `json:"user_id"`
-	Role Role `json:"role"`
-	CreatedAt time.Time `json:"created_at"`
-	ExpiredAt time.Time `json:"expired_at"`
+	ID          int64     `json:"-"`
+	AccessToken string    `json:"access_token"`
+	UserID      int64     `json:"user_id"`
+	Role        Role      `json:"role"`
+	CreatedAt   time.Time `json:"created_at"`
+	ExpiredAt   time.Time `json:"expired_at"`
+}
+
+func (s *Session) IsAccessTokenExpired() bool {
+	if s == nil {
+		logrus.Warn("function: IsAccessTokenExpired is called on nil Session")
+		return true
+	}
+
+	now := time.Now()
+	return now.After(s.ExpiredAt)
 }
 
 type SessionRepository interface {
 	Create(ctx context.Context, session *Session) error
 	FindByID(ctx context.Context, sessionID int64) (*Session, error)
+	FindByAccessToken(ctx context.Context, token string) (*Session, error)
 }
