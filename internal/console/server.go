bfd42aca0c15bc07ae39f9a3b1961f792e4104f6
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-09-16 12:49:57 +0700

    init server

--- internal/console/server.go
+++ internal/console/server.go
@@ -3,22 +3,62 @@ package console
 import (
 	"fmt"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/luckyAkbar/bigio-students-monitoring/auth"
+	"github.com/luckyAkbar/bigio-students-monitoring/internal/config"
+	"github.com/luckyAkbar/bigio-students-monitoring/internal/db"
+	"github.com/luckyAkbar/bigio-students-monitoring/internal/delivery/rest"
+	"github.com/luckyAkbar/bigio-students-monitoring/internal/repository"
+	"github.com/luckyAkbar/bigio-students-monitoring/internal/usecase"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var serverCmd = &cobra.Command{
-	Use: "server",
+	Use:   "server",
 	Short: "start the serveer",
-	Long: "start the server",
-	Run: server,
-
+	Long:  "start the server",
+	Run:   server,
 }
 
 func init() {
-	RootCMD.AddCommand(serverCmd);
+	RootCMD.AddCommand(serverCmd)
 }
 
 func server(cmd *cobra.Command, args []string) {
 	setupLogger()
-	fmt.Println("ok")
+	db.InitializePostgresConn()
+
+	sqlDB, err := db.PostgresDB.DB()
+	if err != nil {
+		logrus.Fatal("unable to start server. reason: ", err.Error())
+	}
+
+	defer sqlDB.Close()
+
+	sessionRepo := repository.NewSessionRepo(db.PostgresDB)
+	userRepo := repository.NewUserRepository(db.PostgresDB)
+
+	authUsecase := usecase.NewAuthUsecase(sessionRepo, userRepo)
+
+	authMiddleware := auth.NewMiddleware(sessionRepo, userRepo)
+
+	HTTPServer := echo.New()
+
+	HTTPServer.Pre(middleware.AddTrailingSlash())
+
+	HTTPServer.Use(middleware.Logger())
+	HTTPServer.Use(authMiddleware.UserSessionMiddleware())
+	HTTPServer.Use(authMiddleware.RejectUnauthorizedRequest())
+
+	RESTGroup := HTTPServer.Group("rest")
+
+	rest.NewRESTService(RESTGroup, authUsecase)
+
+	if err := HTTPServer.Start(fmt.Sprintf(":%s", config.ServerPort())); err != nil {
+		logrus.Fatal("unable to start server. reason: ", err.Error())
+	}
+
+	logrus.Info("Server running on port: ", config.ServerPort())
 }
